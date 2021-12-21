2423ce1fa019398eab0f962e7734f54544de42b2
won-developer <gw4609@gmail.com>
2021-12-21 21:53:29 +0900

    [검증2 - Bean Validation] Bean Validation - 에러 코드

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -16,14 +16,49 @@ public class Item {
     private Long id;
 
     //@NotBlank(message = "공백X") //빈값 + 공백만 있는경우를 허용x
+    /*
+        ErrorCodeMessage Level
+            Level1: NotBlank.item.itemName
+            Level2: NotBlank.itemName
+            Level3: NotBlank.java.lang.String
+            Level4: NotBlank
+     */
     @NotBlank
     private String itemName;
 
+    /*
+        ErrorCodeMessage Level
+            Level1: NotNull.item.price
+            Level2: NotNull.price
+            Level3: NotNull.java.lang.Integer
+            Level4: NotNull
+    */
     @NotNull //null 허용x
+    /*
+        ErrorCodeMessage Level
+            Level1: Range.item.price
+            Level2: Range.price
+            Level3: Range.java.lang.Integer
+            Level4: Range
+    */
     @Range(min = 1000, max = 1000000) //1000~1000000사이의 값만 허용
     private Integer price;
 
+    /*
+        ErrorCodeMessage Level
+            Level1: NotNull.item.price
+            Level2: NotNull.price
+            Level3: NotNull.java.lang.Integer
+            Level4: NotNull
+    */
     @NotNull //null 허용x
+    /*
+        ErrorCodeMessage Level
+            Level1: Max.item.price
+            Level2: Max.price
+            Level3: Max.java.lang.Integer
+            Level4: Max
+    */
     @Max(9999) //최대가 9999
     private Integer quantity;
 
