b76e550120afe1d635d53cf8ca6c5314a61559e0
won-developer <gw4609@gmail.com>
2022-01-19 20:39:44 +0900

    [검증2 - Bean Validation] Bean Validation - 한계-1

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,11 +1,9 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-
 //hibernate에서만 제공하는 validation, 즉 hibernate를 구현체로하는 validation만 사용가능.
 import org.hibernate.validator.constraints.Range;
 import org.hibernate.validator.constraints.ScriptAssert;
-
 //javax에서 제공하는 표준 validation. 구현체에 관계없이 제공되는 표준 인터페이스.
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
@@ -22,54 +20,24 @@ import javax.validation.constraints.NotNull;
 /*@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000",
 message = "총합이 10,000원 넘게 입력해주세요.")*/
 public class Item {
-
-    @NotNull //수정 요구사항 추가
+    /*
+    ErrorCodeMessage Level Annotation별로 공통이다.
+        Level1: (Annotation명).item.itemName ex)NotBlank.item.itemName
+        Level2: (Annotation명).itemName
+        Level3: (Annotation명).java.lang.String, Integer 타입일 경우 NotBlank.java.lang.Integer
+        Level4: (Annotation명)
+    */
+    @NotNull
     private Long id;
 
-    //@NotBlank(message = "공백X") //빈값 + 공백만 있는경우를 허용x
-    /*
-        ErrorCodeMessage Level
-            Level1: NotBlank.item.itemName
-            Level2: NotBlank.itemName
-            Level3: NotBlank.java.lang.String
-            Level4: NotBlank
-     */
     @NotBlank
     private String itemName;
 
-    /*
-        ErrorCodeMessage Level
-            Level1: NotNull.item.price
-            Level2: NotNull.price
-            Level3: NotNull.java.lang.Integer
-            Level4: NotNull
-    */
-    @NotNull //null 허용x
-    /*
-        ErrorCodeMessage Level
-            Level1: Range.item.price
-            Level2: Range.price
-            Level3: Range.java.lang.Integer
-            Level4: Range
-    */
+    @NotNull
     @Range(min = 1000, max = 1000000) //1000~1000000사이의 값만 허용
     private Integer price;
 
-    /*
-        ErrorCodeMessage Level
-            Level1: NotNull.item.price
-            Level2: NotNull.price
-            Level3: NotNull.java.lang.Integer
-            Level4: NotNull
-    */
-    @NotNull //null 허용x
-    /*
-        ErrorCodeMessage Level
-            Level1: Max.item.price
-            Level2: Max.price
-            Level3: Max.java.lang.Integer
-            Level4: Max
-    */
+    @NotNull
     //@Max(9999) //수정 요구사항 추가
     private Integer quantity;
 
